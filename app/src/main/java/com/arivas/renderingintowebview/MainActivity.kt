08f9a972573cedec256cdada8b2f625360921fc0
Andres Rivas <arivas@zetech.com.ar>
2018-06-05 17:18:22 -0300

    First commit, add rendering of web page into webview

--- app/src/main/java/com/arivas/renderingintowebview/MainActivity.kt
+++ app/src/main/java/com/arivas/renderingintowebview/MainActivity.kt
@@ -2,11 +2,18 @@ package com.arivas.renderingintowebview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadData()
     }
+
+    private fun loadData(){
+        webview_upload.loadUrl("https://www.google.com/")
+    }
+
 }
